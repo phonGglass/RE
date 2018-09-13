1c3e1c84d64c9ed549f6ac8e075b2867326dd58c
Trung Nguyen <Trungghost.win@gmail.com>
2018-09-13 22:44:23 +0700

    gitflow-feature-stash: editContronller
    Edit controller dashboard

---
+++ RE_PROJECT/RE/Areas/Administrators/Controllers/DashboardsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RE.Areas.Administrators.Controllers
+{
+    public class DashboardsController : Controller
+    {
+        // GET: Administrators/Dashboards
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
