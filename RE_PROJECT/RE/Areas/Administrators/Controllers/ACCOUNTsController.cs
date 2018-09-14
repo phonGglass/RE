de2f795f9754362ca6bfc969385a7f1d54de95bd
Trung Nguyen <Trungghost.win@gmail.com>
2018-09-15 00:38:11 +0700

    managerAcount
    kết nối với csdl + thêm controller admin + xử lý trang Index , thêm, xóa, sửa, xem chi tiết  account

---
+++ RE_PROJECT/RE/Areas/Administrators/Controllers/ACCOUNTsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RE.Models;
+
+namespace RE.Areas.Administrators.Controllers
+{
+    public class ACCOUNTsController : Controller
+    {
+        private RealEstateEntities db = new RealEstateEntities();
+
+        // GET: Administrators/ACCOUNTs
+        public ActionResult Index()
+        {
+            return View(db.ACCOUNTs.ToList());
+        }
+
+        // GET: Administrators/ACCOUNTs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ACCOUNT aCCOUNT = db.ACCOUNTs.Find(id);
+            if (aCCOUNT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aCCOUNT);
+        }
+
+        // GET: Administrators/ACCOUNTs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administrators/ACCOUNTs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "accID,userName,passWords,phone,email,emailBackup1,emailBackup2,zaloLink,session")] ACCOUNT aCCOUNT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ACCOUNTs.Add(aCCOUNT);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aCCOUNT);
+        }
+
+        // GET: Administrators/ACCOUNTs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ACCOUNT aCCOUNT = db.ACCOUNTs.Find(id);
+            if (aCCOUNT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aCCOUNT);
+        }
+
+        // POST: Administrators/ACCOUNTs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "accID,userName,passWords,phone,email,emailBackup1,emailBackup2,zaloLink,session")] ACCOUNT aCCOUNT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aCCOUNT).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aCCOUNT);
+        }
+
+        // GET: Administrators/ACCOUNTs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ACCOUNT aCCOUNT = db.ACCOUNTs.Find(id);
+            if (aCCOUNT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aCCOUNT);
+        }
+
+        // POST: Administrators/ACCOUNTs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ACCOUNT aCCOUNT = db.ACCOUNTs.Find(id);
+            db.ACCOUNTs.Remove(aCCOUNT);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- RE_PROJECT/RE/Areas/Administrators/Controllers/DashboardsController.cs
+++ RE_PROJECT/RE/Areas/Administrators/Controllers/DashboardsController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RE.Models;
 
 namespace RE.Areas.Administrators.Controllers
 {
     public class DashboardsController : Controller
     {
+        private RealEstateEntities db = new RealEstateEntities();
         // GET: Administrators/Dashboards
         public ActionResult Index()
         {
-            return View();
+            
+            return View(db.ACCOUNTs.ToList());
         }
     }
 }
